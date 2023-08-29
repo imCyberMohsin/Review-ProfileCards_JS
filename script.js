e4a9703854fe3f83240eb57918288a1550349eff
imCyberMohsin <mohsinansari7033@gmail.com>
2023-08-29 19:16:24 +0530

    css done

---
+++ script.js
@@ -0,0 +1,32 @@
+//* Review Profile Cards Script
+
+//? Dynamic Card Data
+const cards = [
+    {
+        id: 1,
+        name: 'Elliot',
+        job: 'Black Hat Hacker',
+        img: './img/image1.jpg',
+        text: "I'm Eliot, a proficient black hat hacker, navigates digital realms with clandestine expertise, exploiting vulnerabilities for personal gain and expose people who are vulnearable for the society."
+    },
+    {
+        id: 2,
+        name: 'Altair',
+        job: 'Black Hat Hacker',
+        img: './img/image1.jpg',
+        text: "I'm Eliot, a proficient black hat hacker, navigates digital realms with clandestine expertise, exploiting vulnerabilities for personal gain and expose people who are vulnearable for the society."
+    }
+    
+    // Add more here....
+];
+
+//? Getting HTML Elements
+const leftBtn = document.querySelector('.left-right');
+const rightBtn = document.querySelector('.right-btn');
+const shuffleBtn = document.querySelector('.random-btn');
+
+
+//? Functions to Display the cards Dynamically
+const displayCard = () => {
+
+} 
